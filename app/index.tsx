3fc1f553ad438dc03fed612c065986f76ec96214
NokaK <nokakipiani93@gmail.com>
2024-04-08 19:46:10 +0400

    remove unused tabs

---
+++ app/index.tsx
@@ -0,0 +1,36 @@
+import { Link, Stack } from "expo-router";
+import { StyleSheet } from "react-native";
+
+import { Text, View } from "@/components/Themed";
+import HomeScreen from "@/app/HomeScreen";
+const home = () => {
+  return (
+    <>
+      <View style={styles.container}>
+        <HomeScreen />
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+  linkText: {
+    fontSize: 14,
+    color: "#2e78b7",
+  },
+});
+export default home;
